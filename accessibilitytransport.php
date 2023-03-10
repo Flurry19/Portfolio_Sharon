d9316d5434f6ae402087736a9e1c8034a84cad84
Flurry19 <sharonirenedewit@gmail.com>
2023-03-10 13:14:36 +0100

    edit

---
+++ accessibilitytransport.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/chat.css">
+    <script src="js/chat.js"></script>
+</head>
+
+<body>
+<!-- Chatbox -->
+<!--<button class="open-button" onclick="openForm()">Chat</button>-->
+<!-- Begin Section -->
+<!--<section class="chat-popup" id="myForm">-->
+<!--    <form action="mailto:sharonirenedewit@gmail.com" method="phost" class="form-container">-->
+<!--        <h1>Chat</h1>-->
+<!--        <label for="msg"><b>Bericht:</b></label>-->
+<!--        <textarea placeholder="Type hier" name="msg" required></textarea>-->
+<!--        <button type="submit" class="submit btn">Verstuur</button>-->
+<!--        <button type="button" class="close btn" onclick="closeForm()">Sluiten</button>-->
+<!--    </form>-->
+<!--</section>-->
+<!-- Begin Navigation -->
+<nav>
+    <!-- Begin Section -->
+    <section class="nav-container">
+        <a href="index.html#home">Home</a>
+        <a href="index.html#overmij">Over mij</a>
+        <a href="index.html#projecten">Projecten</a>
+        <a href="index.html#hobby">Hobby's</a>
+        <a href="contact.html#contact">Contact</a>
+    </section>
+</nav>
+<!-- Begin Header -->
+<header>
+    <img class="bigimageproject3" id="home" src="images/tegel-motorisch.jpg">
+    <h2 class="projecttitle">Accessibility Applicatie OV Rolstoel</h2>
+</header>
+<!-- Begin Main -->
+<main>
+    <!-- Begin Section -->
+    <section class="container">
+        <h2>Startproject</h2>
+        <div class="projectcontainer">
+            <figure>
+            <img class="cle3" src="images/IMG_20230215_113056.jpg" alt="Project Kat Sprint 2">
+            <img class="cle3" src="images/IMG_20230215_143222.jpg" alt="Project Kat Sprint 2">
+            </figure>
+                <p>Het project begon toen wij de opdracht kregen om voor onze doelgroep en onze omgeving een applicatie te ontwikkelen. Onze doelgroep bestond uit motorisch beperkten en onze omgeving was het openbaar vervoer.</p>
+        </div>
+    </section>
+    <!-- Begin Section -->
+    <section class="container">
+        <h2>Proces</h2>
+        <div class="projectcontainer">
+            <p>Maar hoe kom je er nou achter wat je kan verbeteren voor motorisch beperkten in het openbaar vervoer? Buiten dat wij een interview gedaan hebben met iemand in een rolstoel zijn we er ook op uitgegaan en zijn we het zelf gaan ervaren. We hebben de intercity, bus, tram en metro geprobeerd. De tram vonden we erg goed te doen, omdat zelfs van tevoren bij het station aangegeven stond welke ingangen rolstoeltoegankelijk waren. De intercity was makkelijk om in te stappen, alleen hier stond dit niet aangegeven dus was het gokken op een deur. Bij de bus merkten we al snel dat we hulp moesten vragen aan iemand of zij de klep naar beneden kon doen. Het ergste was de metro. We kwamen aan bij een station en de lift werkte niet. Dit was echter de enige lift op het station, we konden dus niet naar boven en we konden nergens van tevoren zien of de lift werkte of niet. </p>
+        </div>
+
+    </section>
+    <!-- Begin Section -->
+    <section class="container">
+        <h2>Applicatie</h2>
+        <section class="projectcontainer">
+            <img src="images/Wireframe%20applicatie.jpg" alt="Wireframe Applicatie">
+            <p>Aan de hand van het proces wat we doorlopen hebben, hebben we gemerkt wat de problemen waren van rolstoelgebruik in het openbaar vervoer. Daarom wilde we een website maken waarop een kaart staat met alle stations en haltes in Rotterdam. Hoewel hierboven nog alleen de wireframe te zien is, is het plan om de link van de website te plaatsen. We gaan dus een webapplicatie maken met een kaart waarop allerlei stations zijn. Per station kan je zien hoe druk het op het moment is, of de liften werken, of het station ondergronds of bovengronds is en of het rolstoeltoegankelijk is. Daarbij geven we ook nog de mogelijkheid om reviews achter te laten. Ondanks dat er namelijk wordt aangegeven dat een station rolstoeltoegankelijk is, kan het zijn dat er een tegel scheef ligt en je er toch niet kan komen met de rolstoel. Daarom laten we mensen reviews achterlaten per station. </p>
+        </section>
+    </section>
+    <!-- Begin Section -->
+    <section class="container">
+        <h2>Eindresultaat</h2>
+        <section class="projectcontainer">
+            <p>Hier komt de link van de website eenmaal hij online staat. </p>
+        </section>
+    </section>
+</main>
+<!-- Begin Footer -->
+<footer>
+    <h3 id="socials">Socials</h2>
+        <!-- Begin Section -->
+        <section class="social-container">
+            <a class="icons" href="https://www.instagram.com/databaseofdoggos/"><img src="images/Instagram-Logo.png"
+                                                                                     class="instagram" alt="Instagram Logo"></a>
+            <a class="icons" href="https://www.etsy.com/nl/shop/Nerdyglazen?ref=seller-platform-mcnav"><img
+                    src="images/Etsy-Logo.png" class="etsy" alt="Etsy Logo"></a>
+            <a class="icons" href="https://www.facebook.com/sharon.dewit.7587/"><img
+                    src="images/Facebook_icon_2013.svg.webp" class="facebook" alt="Facebook Logo"></a>
+        </section>
+</footer>
+
+</body>
+
+</html>
